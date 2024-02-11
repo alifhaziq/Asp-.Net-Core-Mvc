4189b01b90916f2422e0cc207bb96e8f3c506866
Afiq Mustaffa <afiq.mustaffa@cloud-connect.asia>
2024-02-11 23:04:36 +0800

    Add checkbox binding

--- WebRedis2/Models/Application3.cs
+++ WebRedis2/Models/Application3.cs
@@ -2,6 +2,8 @@
 {
     public class Application3
     {
-
+        public List<string> Checkboxes { get; set; } = new List<string>();
+        public string US { get; set; }
+        public string NotUs { get; set; }
     }
 }
